b5f54226386ce3f27fba489902f596a017bc8c66
vorhitALONE <117316408+vorhitALONE@users.noreply.github.com>
2025-12-13 02:26:32 +0300

    Create config table in database if not exists
    Added SQL commands to create config table if it doesn't exist.

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ const db = new Database(DB_PATH);
 const initSql = fs.readFileSync(path.join(__dirname, 'migrations', 'init.sql'), 'utf8');
 db.exec(initSql);
 
-module.exports = db;
+// Создаём таблицу config если её нет
 db.exec(`
   CREATE TABLE IF NOT EXISTS config (
     id INTEGER PRIMARY KEY,
@@ -22,3 +22,5 @@ db.exec(`
   
   INSERT OR IGNORE INTO config (id, active_value) VALUES (1, NULL);
 `);
+
+module.exports = db;
